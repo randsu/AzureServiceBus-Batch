97c5aa6119d2a59749a8a02bcc7b64be6d24e266
Richard Sundby Anderssen <trollbyte@gmail.com>
2024-02-08 15:05:08 +0100

    Support entering azure service bus connectionstring, clarify payload

--- AzureFunction.QueueBatchProcessing.Sender/Program.cs
+++ AzureFunction.QueueBatchProcessing.Sender/Program.cs
@@ -4,20 +4,42 @@ using Azure.Identity;
 using Azure.Messaging.ServiceBus;
 using AzureFunction.QueueBatchProcessing.Models;
 
-var client = new ServiceBusClient("Paste the SB con string here");
-var sender = client.CreateSender("lab-queue");
+var experimentalQueueConnectionString = "Endpoint=sb://experimental-rsa.servicebus.windows.net/;SharedAccessKeyName=manage;SharedAccessKey=kDyuHPy4W0FRB/FMLicuQSff/hb6lQ2gi+ASbBZ+uf4=;EntityPath=experimental-queue";
+
+Console.WriteLine("ServiceBus connectionstring or enter for default:");
+var connString = Console.ReadLine();
+if (string.IsNullOrEmpty(connString))
+    connString = experimentalQueueConnectionString;
+
+var client = new ServiceBusClient(connString);
+
+var entityPath = connString.Split(';')?.Last()?.Split('=')?.Last();
+if(string.IsNullOrEmpty(entityPath))
+{
+    Console.WriteLine("EntityPath or enter for default:");
+    entityPath = Console.ReadLine();
+}
+
+var sender = client.CreateSender(entityPath);
 using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
 
 const int numOfMessages = 15;
 for (int i = 1; i <= numOfMessages; i++)
 {
-    if (!messageBatch.TryAddMessage(new ServiceBusMessage(new Order(Guid.NewGuid(), numOfMessages * 10, DateTime.Now).ToString())))
+    var payload =
+        new Order(
+            Guid.NewGuid(),
+            (int)Math.Floor(numOfMessages * i * 0.3d),
+            DateTime.Now
+        ).ToString();
+
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage(payload)))
         throw new Exception($"The message {i} is too large to fit in the batch.");
 }
 try
 {
     await sender.SendMessagesAsync(messageBatch);
-    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the servicebus namespace.");
 }
 finally
 {
@@ -27,4 +49,3 @@ finally
 
 Console.WriteLine("Press any key to end the application");
 Console.ReadKey();
-
